f3fb1eb685ee7a2fb86c4702a41fc1eafc6ff30d
Eugeniu Plamadeala <eugeniu@plamadeala.com>
2018-01-28 22:09:45 -0600

    Fully working, some correctness issues in corner cases.

---
+++ lab5/KdTree.java
@@ -0,0 +1,13 @@
+/******************************************************************************
+ *  Name:    Eugeniu Plamadeala
+ *  NetID:   eugeniu
+ *  Precept: P01
+ *
+ *  Partner Name:    N/A
+ *  Partner NetID:   N/A
+ *  Partner Precept: N/A
+ *
+ *  Description: Represents a set of points in the unit square.
+ *  supports a range search, and nearest neighbor search.
+ *  Uses a kd-tree internally to store the points.
+ ******************************************************************************/
--- lab5/PointSET.java
+++ lab5/PointSET.java
@@ -7,22 +7,23 @@
  *  Partner NetID:   N/A
  *  Partner Precept: N/A
  *
- *  Description:
- *
+ *  Description: Represents a set of points in the unit square.
+ *  supports a range search, and nearest neighbor search.
+ *  Uses a Red-Black tree internally to store the points.
  ******************************************************************************/
 import java.util.TreeSet;
 import java.util.Iterator;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
-
+import edu.princeton.cs.algs4.Stack;
 
 
 public class PointSET {
 
-    private TreeSet root;
+    private TreeSet<Point2D> root;
 
-    public         PointSET() { root = new TreeSet(); }
+    public         PointSET() { root = new TreeSet<Point2D>(); }
 
     public  boolean isEmpty()
     {
@@ -41,18 +42,18 @@ public class PointSET {
         if (p == null) throw new java.lang.IllegalArgumentException();
         if (root == null)
         {
-            root = new TreeSet();
-            root.insert(p);
+            root = new TreeSet<Point2D>();
+            root.add(p);
             return;
         }
 
-        if (! root.contains(p)) root.insert(p);
+        if (! root.contains(p)) root.add(p);
     }
 
     // does the set contain point p?
     public           boolean contains(Point2D p)
     {
-        if (p == null) throw new java.lang.IllegalArgumentException();();
+        if (p == null) throw new java.lang.IllegalArgumentException();
         return (root == null ? false : root.contains(p));
     }
 
@@ -64,7 +65,8 @@ public class PointSET {
 
         while (it.hasNext())
         {
-            StdDraw.println(it.next());
+            Point2D p = it.next();
+            StdDraw.point(p.x(), p.y());
         }
     }
 
@@ -77,9 +79,37 @@ public class PointSET {
     public Iterable<Point2D> range(RectHV rect)
     {
         nullQ(rect);
-        
-    }
-    public           Point2D nearest(Point2D p)             // a nearest neighbor in the set to point p; null if the set is empty
 
-    public static void main(String[] args)                  // unit testing of the methods (optional)
+        Stack<Point2D> stack = new Stack<Point2D>();
+        Iterator<Point2D> it = root.iterator();
+
+        while (it.hasNext())
+        {
+            Point2D p = it.next();
+            if (rect.contains(p)) stack.push(p);
+        }
+        return stack;
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public           Point2D nearest(Point2D p)
+    {
+        nullQ(p);
+        Iterator<Point2D> it = root.iterator();
+        Point2D closest = null;
+        double dist = Double.POSITIVE_INFINITY;
+
+        while (it.hasNext())
+        {
+            Point2D curr = it.next();
+            if (dist > p.distanceTo(curr))
+            {
+                dist = p.distanceTo(curr);
+                closest = curr;
+            }
+        }
+
+        return closest;
+    }
+
 }
